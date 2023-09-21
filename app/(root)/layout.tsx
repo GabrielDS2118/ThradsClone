26c5ddc3c74816f61423c738da132bb050268dbc
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-09-21 16:15:00 -0500

    8.Left Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-// import { dark } from "@clerk/themes";
+import { dark } from "@clerk/themes";
 
 import "../globals.css";
 
@@ -26,11 +26,13 @@ export default function RootLayout({
   return (
     <ClerkProvider
       appearance={{
-        // baseTheme: dark,
+        baseTheme: dark,
       }}
     >
       <html lang='en'>
         <body className={inter.className}>
+
+
           <Topbar />
 
           <main className='flex flex-row'>
